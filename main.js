8950009be1ebbbf4ce151d163e4cca48c917fafa
IPHONE2356 <73042876+IPHONE2356@users.noreply.github.com>
2022-06-20 09:07:03 +0100

    Add main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+//Importing Libraries
+//This is the library that communicates with the Discord API
+const discord = require("Discord.js")
+//This is the library that allows node to read and write to other files
+const fs = reqiore("fs")
+//This is the library which we will use to make web requests to send/recieve information 
+const axios = require("axios")
+//This will be used to execute shell commands on the system
+const { exec } = require("child_process");
+//Defining Functions
+function checkSetup(){
+    
+}
+//Checking to see if this bot's setup files have been created
+
+
+
