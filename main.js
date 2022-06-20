41cd03da33526e0826122be86119c04d4fafea8d
IPHONE2356 <sasadiq1@icloud.com>
2022-06-20 08:53:10 +0000

    Edit main.js

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
+//This will be used to execute shell commands on the system
+const { exec } = require("child_process");
+//This is the library that allows node to read and write to other files
+const fs = require("fs")
+//Defining Functions
+//This is an async function will prevent the lines below the called function being executed before/during the function
+//This can prevent errors as this function installs libraries and the lines beneath needs those libraries 
+async function checkSetup(){
+    await exec("npm install discord.js@v12 axios opusscript");
+
+}
+//Checking to see if this bot's setup files have been created
+checkSetup()
 //Importing Libraries
 //This is the library that communicates with the Discord API
 const discord = require("Discord.js")
-//This is the library that allows node to read and write to other files
-const fs = reqiore("fs")
+
 //This is the library which we will use to make web requests to send/recieve information 
 const axios = require("axios")
-//This will be used to execute shell commands on the system
-const { exec } = require("child_process");
-//Defining Functions
-function checkSetup(){
-    
-}
-//Checking to see if this bot's setup files have been created
 
 
 
