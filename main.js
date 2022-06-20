00003f837ae42880be5cb6ffb00803207df3ee24
IPHONE2356 <sasadiq1@icloud.com>
2022-06-20 11:19:32 +0000

    Add .gitignore

--- main.js
+++ main.js
@@ -6,11 +6,19 @@ const fs = require("fs")
 //This is an async function will prevent the lines below the called function being executed before/during the function
 //This can prevent errors as this function installs libraries and the lines beneath needs those libraries 
 async function checkSetup(){
-    await exec("npm install discord.js@v12 axios opusscript");
+    var output = ""
+    if (fs.existsSync("./node_modules")){
+
+    }
+    //If the folder node_modules does not exist, that means no libraries are installed
+    else{
+        output = "discord.js@v12 axios opusscript"
+    }
+    await exec(`npm install ${output}`);
 
 }
 //Checking to see if this bot's setup files have been created
-checkSetup()
+che1ckSetup()
 //Importing Libraries
 //This is the library that communicates with the Discord API
 const discord = require("Discord.js")
